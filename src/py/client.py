ba5cc5d6bc792d1eb6a7c9a48b24c3ce5c422a75
FANTOME <indv.panh@gmail.com>
2022-03-13 11:38:27 +0000

    Update client.py

--- src/py/client.py
+++ src/py/client.py
@@ -43,6 +43,7 @@ class CifarClient(fl.client.NumPyClient):
     def set_parameters(self, parameters):
         params_dict = zip(self.model.state_dict().keys(), parameters)
         state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        print([o.shape for o in parameters][:8])
         self.model.load_state_dict(state_dict, strict=True)
 
     def fit(self, parameters, config):
