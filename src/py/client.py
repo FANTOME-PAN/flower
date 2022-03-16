ee4ba082e443ab210b201670abebc4813e9c99c3
FANTOME <indv.panh@gmail.com>
2022-03-16 17:25:11 +0000

    update

--- src/py/client.py
+++ src/py/client.py
@@ -74,7 +74,7 @@ class CifarClient(fl.client.NumPyClient):
         print(f"client {self.cid} starts to fit")
         # Train the model
         trainloader = DataLoader(self.trainset, batch_size=batch_size, shuffle=True)
-        train(self.model, trainloader, device=self.device, start_epoch=start_epoch, end_epoch=end_epoch, max_iter=6)
+        train(self.model, trainloader, device=self.device, start_epoch=start_epoch, end_epoch=end_epoch, max_iter=8)
         diff = weights_subtraction(self.get_parameters(), parameters)
         # Run evaluation
         # testloader = DataLoader(self.testset, batch_size=32, shuffle=False)
